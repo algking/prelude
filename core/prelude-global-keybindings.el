cce586c64131b67211c7d9503c75d548192ff611
algking <alg_kingl@hotmail.com>
2013-08-17 11:01:43 +0800

    修改 ace jump key-binding

--- core/prelude-global-keybindings.el
+++ core/prelude-global-keybindings.el
@@ -117,7 +117,7 @@
 ;; key chords
 (require 'key-chord)
 
-(key-chord-define-global "jj" 'ace-jump-word-mode)
+(key-chord-define-global "jo" 'ace-jump-word-mode)
 (key-chord-define-global "jl" 'ace-jump-line-mode)
 (key-chord-define-global "jk" 'ace-jump-char-mode)
 (key-chord-define-global "JJ" 'prelude-switch-to-previous-buffer)
